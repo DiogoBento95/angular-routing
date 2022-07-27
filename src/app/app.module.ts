876d03e1af86ec1fcce4a3073e325f44ca760dd4
Diogo Bento <diogo.bento@bringglobal.com>
2022-07-27 15:51:24 +0000

    feat: Generated the app and 5 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { TheTeamComponent } from './the-team/the-team.component';
+import { ApplyNowComponent } from './apply-now/apply-now.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    TheTeamComponent,
+    ApplyNowComponent
   ],
   imports: [
     BrowserModule,
